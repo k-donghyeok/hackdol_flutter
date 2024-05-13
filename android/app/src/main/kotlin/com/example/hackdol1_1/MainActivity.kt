e98de90cf088a08e4fec406f2badcd8cde83e03c
k-donghyeok <rkdehdgur432@naver.com>
2024-05-13 21:50:27 +0900

    로그인 회원가입 연동
    연동

--- android/app/src/main/kotlin/com/example/hackdol1_1/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/hackdol1_1/MainActivity.kt
@@ -1,27 +1,21 @@
 package com.example.hackdol1_1
 
-import com.example.BlockPhoneNumberHandler
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
+import androidx.annotation.NonNull
 
 
 class MainActivity : FlutterActivity() {
-    fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        GeneratedPluginRegistrant.registerWith(flutterEngine)
-        val methodChannel =
-            MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
-        methodChannel.setMethodCallHandler(
-            BlockPhoneNumberHandler(
-                getContentResolver()
-            )
-        )
+
+
     }
 
     companion object {
-        private const val CHANNEL = "com.example.block_phone_number"
+        //private const val CHANNEL = "com.example.block_phone_number"
     }
 }
 
