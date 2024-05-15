2671e33ce74956f43f04595daf571819eae0352e
k-donghyeok <rkdehdgur432@naver.com>
2024-05-16 03:36:51 +0900

    ㅁ
    ㅁ

--- lib/block_phone_number.dart
+++ lib/block_phone_number.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/services.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class BlockPhoneNumber {
   static const platform = MethodChannel('com.yourapp/block_call');
+  static User? _user;
+  static final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  static Future<void> _getCurrentUser() async {
+
+    if (_auth.currentUser != null) {
+      _user = _auth.currentUser;
+    }
+  }
 
   static Future<void> loadBlockedNumbers() async {
+    _getCurrentUser();
     try {
-      // 여기에 파이어스토어에서 blocknumbers 값을 가져오는 코드를 작성하세요
-      List<String> blockedNumbers = ['01012345678', '01087654321']; // 예시 데이터
-      await platform.invokeMethod('updateBlockedNumbers', blockedNumbers);
-    } on PlatformException catch (e) {
-      print("Failed to load blocked numbers: '${e.message}'.");
+      DocumentSnapshot userDoc = await FirebaseFirestore.instance.collection('users').doc(_user!.uid).get();
+      if (userDoc.exists) {
+        List<dynamic> blockedNumbers = userDoc.get('blockedNumbers') ?? [];
+        print("Blocked numbers loaded: $blockedNumbers"); // 로그로 출력
+        await platform.invokeMethod('updateBlockedNumbers', blockedNumbers.cast<String>());
+      }
+    } catch (e) {
+      print('Error loading blocked numbers: $e');
     }
   }
 }
